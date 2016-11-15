fa6b2168964d279d25883f761ff109b3ab5fdf3c
baguwka <ttehbb@gmail.com>
2016-11-15 18:45:13 +0300

    vk authorization

--- VK Post Organizer/ViewModels/MainVM.cs
+++ VK Post Organizer/ViewModels/MainVM.cs
@@ -3,17 +3,27 @@ using System.Windows;
 using System.Windows.Input;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
+using vk.Models;
 using vk.Views;
 
 namespace vk.ViewModels {
    class MainVM : BindableBase, IVM {
+
+
+      private string _content;
       public ICommand ConfigureContentCommand { get; set; }
       public ICommand UploadCommand { get; set; }
+      public ICommand AuthorizeCommand { get; set; }
+
+      public string Content {
+         get { return _content; }
+         set { SetProperty(ref _content, value); }
+      }
 
       public MainVM() {
          ConfigureContentCommand = new DelegateCommand(configureContentCommandExecute);
          UploadCommand = new DelegateCommand(uploadCommandExecute);
-
+         AuthorizeCommand = new DelegateCommand(authorizeCommandExecute);
       }
 
       private void configureContentCommandExecute() {
@@ -24,6 +34,18 @@ namespace vk.ViewModels {
       private void uploadCommandExecute() {
          MessageBox.Show("This function is not implemented yet", "FYI", MessageBoxButton.OK, MessageBoxImage.Hand);
       }
+
+
+      //https://vk.com/testingchamber
+
+      private void authorizeCommandExecute() {
+         var token = new AccessToken();
+         var authWindow = new AuthView(token);
+         authWindow.ShowDialog();
+
+         MessageBox.Show(token.Code);
+      }
+
 
       public void OnLoad() {
          throw new NotImplementedException();
